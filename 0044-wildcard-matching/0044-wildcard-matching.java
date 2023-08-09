28b8b4b01a292b5c656bdffbb754da282b43523e
Ayush Prince <princeayush04@gmail.com>
2023-08-09 23:36:36 +0530

    Time: 12 ms (88.53%), Space: 46.5 MB (41.96%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,19 +1,21 @@
 class Solution {
-    Boolean[][] dp;;
+    Boolean[][] dp;
+    int n, m;
+    char[] a, b;
     public boolean isMatch(String s, String p) {
-        char[] a = s.toCharArray();
-        char[] b = p.toCharArray();
-        int n = a.length;
-        int m = b.length;
+        a = s.toCharArray();
+        b = p.toCharArray();
+        n = a.length;
+        m = b.length;
         dp = new Boolean[n + 1][m + 1];
-        return recur(0, 0, n, m, a, b);
+        return recur(0, 0);
     }
-    public boolean recur(int i, int j, int n, int m, char[] a, char[] b) {
+    public boolean recur(int i, int j) {
         if(i >= n && j >= m) return true;
         if(i > n || j >= m) return false;
         if(dp[i][j] != null) return dp[i][j];
-        if(b[j] == '*') return dp[i][j] = recur(i + 1, j, n, m, a, b) | recur(i, j + 1, n, m, a, b);
-        if(i < n && (a[i] == b[j] || b[j] == '?')) return dp[i][j] = recur(i + 1, j + 1, n, m, a, b);
+        if(b[j] == '*') return dp[i][j] = recur(i + 1, j) | recur(i, j + 1);
+        if(i < n && (a[i] == b[j] || b[j] == '?')) return dp[i][j] = recur(i + 1, j + 1);
         return dp[i][j] = false;
     }
 }
