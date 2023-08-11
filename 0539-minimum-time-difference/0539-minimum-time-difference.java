af0080a5c65f122967b208b7e98ec78bf698181a
Ayush Prince <princeayush04@gmail.com>
2023-08-12 01:17:04 +0530

    Time: 7 ms (57.65%), Space: 44.6 MB (36.67%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.java
+++ 0539-minimum-time-difference/0539-minimum-time-difference.java
@@ -8,7 +8,7 @@ class Solution {
             a[i] = (Integer.valueOf(s[0]) * 60) + Integer.valueOf(s[1]);
         }
         Arrays.sort(a);
-        int ans = (a[0] - a[n - 1] + (24 * 60)) % (24 * 60);
+        int ans = (a[0] - a[n - 1] + 1440) % 1440;
         for(int i = 0; i<n - 1; i++) ans = min(ans, a[i + 1] - a[i]);
         return ans;
     }
