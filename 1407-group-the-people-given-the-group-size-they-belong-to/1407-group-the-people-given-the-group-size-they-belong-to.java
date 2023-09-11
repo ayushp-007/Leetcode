c889702dd1b0830bd320abaebfebb32725349aa8
Ayush Prince <princeayush04@gmail.com>
2023-09-11 10:51:11 +0530

    Time: 5 ms (93.77%), Space: 44.1 MB (94.45%) - LeetHub

--- 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.java
+++ 1407-group-the-people-given-the-group-size-they-belong-to/1407-group-the-people-given-the-group-size-they-belong-to.java
@@ -2,21 +2,22 @@ class Solution {
     public List<List<Integer>> groupThePeople(int[] groupSizes) {
         List<List<Integer>> ans = new ArrayList<>();
         int n = groupSizes.length;
-        for(int i = 1; i<=n; i++) {
+        List<Integer>[] group = new List[n + 1];
+        for(int i = 1; i <= n; i++) group[i] = new ArrayList<>();
+        for(int i = 0; i < n; i++) {
+            group[groupSizes[i]].add(i);
+
+        }
+        for(int i = 1; i <=n; i++) {
+            int t = group[i].size();
             List<Integer> temp = new ArrayList<>();
-            int size = 0;
-            for(int j = 0; j < n; j++) {
-                if(size == i) {
+            for(int j = 0; j < t; j++) {
+                temp.add(group[i].get(j));
+                if(temp.size() == i) {
                     ans.add(temp);
                     temp = new ArrayList<>();
-                    size = 0;
-                }
-                if(groupSizes[j] == i) {
-                    temp.add(j);
-                    size++;
                 }
             }
-            if(size != 0) ans.add(temp);
         }
         return ans;
     }
