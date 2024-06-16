b1b89e6a47cb13b69e687c66a23724877c34dc29
Ayush Prince <princeayush04@gmail.com>
2024-06-16 23:20:26 +0530

    Time: 0 ms (100%), Space: 44.5 MB (6.79%) - LeetHub

---
+++ 0330-patching-array/0330-patching-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minPatches(int[] nums, int n) {
+        long miss = 1;
+        int ans = 0, i = 0;
+        while(miss <= n) {
+            if(i < nums.length && nums[i] <= miss) {
+                miss += nums[i++];
+            }
+            else {
+                miss += miss;
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
