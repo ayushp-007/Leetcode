3d8d6cf48909e5bcf2c69bebe9d557125490ca11
Ayush Prince <princeayush04@gmail.com>
2024-08-22 22:42:39 +0530

    Time: 0 ms (100%), Space: 39.8 MB (97.89%) - LeetHub

--- 0476-number-complement/0476-number-complement.java
+++ 0476-number-complement/0476-number-complement.java
@@ -1,9 +1,5 @@
 class Solution {
     public int findComplement(int num) {
-        int ans = 0;
-        for(int i = 31 - Integer.numberOfLeadingZeros(num); i >= 0; i--) {
-            ans |= (1 - ((num >> i) & 1)) << i;
-        }
-        return ans;
+        return ((1 << (31 - Integer.numberOfLeadingZeros(num) + 1)) - 1) ^ num;
     }
 }
